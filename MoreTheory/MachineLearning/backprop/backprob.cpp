c2c9a99df680e396f6173765b854f4250631d2ed
Gricha1 <64344132+Gricha1@users.noreply.github.com>
2022-03-03 03:16:00 +0400

    Add files via upload

---
+++ MoreTheory/MachineLearning/backprop/backprob.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+class sigmoid_node{
+    public:
+        int forward(){
+
+        }
+
+        int backward(){
+
+        }
+}
+*/
+
+class hidden_node{
+    public:
+        int forward(){
+
+
+        }
+
+        int backward(int dz){
+            //dz = dL / dz
+            dx, dz = 
+            
+        }
+}
+
+
+
+
+
+
+int main(){
+
+    model.backprop();
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
